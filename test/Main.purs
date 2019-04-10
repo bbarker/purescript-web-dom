e34adf4b409689e2d4e411e37d8c920852e43a80
Brandon Elam Barker <beb82@cornell.edu>
2019-04-10 18:26:41 +0000

    adding testing framework, but not testing xpath code yet

---
+++ test/Main.purs
@@ -0,0 +1,15 @@
+module Test.Main where
+
+import Prelude
+
+import Test.Unit (suite, test, timeout)
+import Test.Unit.Main (runTest)
+import Test.Unit.Assert as Assert
+
+main = runTest do
+  suite "sync code" do
+    test "arithmetic" do
+      Assert.assert "2 + 2 should be 4" $ (2 + 2) == 4
+      Assert.assertFalse "2 + 2 shouldn't be 5" $ (2 + 2) == 5
+      Assert.equal 4 (2 + 2)
+      Assert.expectFailure "2 + 2 shouldn't be 5" $ Assert.equal 5 (2 + 2)
